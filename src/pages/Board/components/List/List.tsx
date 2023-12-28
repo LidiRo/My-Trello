c99086172da14361a99d278ce11781dee93c2c59
Lidiia <113384507+LidiRo@users.noreply.github.com>
2023-12-28 21:21:14 +0200

    d&d

--- src/pages/Board/components/List/List.tsx
+++ src/pages/Board/components/List/List.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { createElement, useEffect, useState } from "react";
 import './list.scss';
 import { Card } from "../Card/Card";
 import CreateNewCard from "../Card/CreateNewCard/CreateNewCard";
 import { ICard } from "../../../../common/interfaces/ICard";
 import { Slot } from "../Slot/Slot";
-import { dragStartReducer } from "../../../../store/reducers/SlotSlice";
+import { dragStartReducer, getCoordinatesCard } from "../../../../store/reducers/SlotSlice";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 
 export const List = (props: {
@@ -18,6 +18,8 @@ export const List = (props: {
 }) => {
     const dispatch = useAppDispatch();
     const { cards } = props;
+    const arrCards = [...cards];
+    // console.log(arrCards)
     const [isMouseEnter, setIsMouseEnter] = useState(false);
 
     const position = cards?.length ? cards.length + 1 : 1;
@@ -37,24 +39,42 @@ export const List = (props: {
 
     const [isVisibleSlot, setIsVisibleSlot] = useState<boolean[]>(cards.map(() => false));
 
-    const { draggingCard } = useAppSelector(state => state.slots)
+    const { draggingCard, coordinatesCard } = useAppSelector(state => state.slots)
+
+    const [isDraggingElement, setIsDraggindElement] = useState<HTMLElement | null>(null);
 
     //************DRAGGIND CARD****************/
 
     function dragStart(e: any, card: ICard) {
-        console.log('Start');
+        // console.log('Start');
         const draggingElement = e.currentTarget;
-        e.dataTransfer.setData('text/plaid', draggingElement.id)
+        const coordinatesDraggingElement = draggingElement.getBoundingClientRect();
+        const coordinates = {
+            top: coordinatesDraggingElement.top,
+            bottom: coordinatesDraggingElement.bottom,
+            height: coordinatesDraggingElement.height,
+            width: coordinatesDraggingElement.width,
+            y: coordinatesDraggingElement.y,
+        }
+
+        // e.dataTransfer.setDragImage(draggingElement, coordinatesDraggingElement.width / 2, coordinatesDraggingElement.height / 2)
+        // console.log(coordinates.y);
+        setIsDraggindElement(draggingElement);
+        // e.dataTransfer.setData('text/plaid', draggingElement.id)
         setTimeout(() => {
             draggingElement.classList.add('dragging');
-        })
+        }, 0)
+
         dispatch(dragStartReducer(card))
+        dispatch(getCoordinatesCard(coordinates));
+
+        
     }
 
     function dragEnd(e: any) {
-        console.log('End');
+        // console.log('End');
         e.target.classList.remove('dragging');
-        setIsVisibleSlot(cards.map((card) => false))
+        // setIsVisibleSlot(cards.map((card) => false))
     }
 
     //************DROP ZONE****************/
@@ -62,42 +82,69 @@ export const List = (props: {
     function dragOver(e: any, container: any) {
         e.preventDefault();
     }
+    
 
-    function dragEnter(e: any, card: ICard) {
+    const slot = document.createElement("div");
+    slot.classList.add("slot-visible");
+
+    function dragEnter(e: any) {
         e.preventDefault();
-        if (!draggingCard) return;
-        if (card.id === draggingCard.id) return;
-
-        const box = e.currentTarget.getBoundingClientRect();
-        const centerBox = box.top + box.height / 2;
-        if (centerBox < e.clientY) {
-            setIsVisibleSlot(isVisibleSlot.map((_, index: number) => {
-                if (draggingCard?.position) {
-                    if (index === draggingCard.position - 1) {
-                        return true;
-                    }
-                }
-                return false
-            }))
-        }
-
-        if (centerBox >= e.clientY) {
-            setIsVisibleSlot(isVisibleSlot.map((_, index: number) => {
-                if (draggingCard?.position) {
-                    if (index === draggingCard.position) {
-                        return true;
-                    }
-                }
-                return false
-            }))
-        }
-    }
-
-    function dragLeave(e: any,) {
+        // console.log(coordinatesCard.y)
+        // console.log('Enter', e.currentTarget)
         
+        // const newArr = [...cards];
+        // console.log("newArr 1", arrCards);
+        // arrCards.push(arrCards[0]);
+        // console.log("newArr 2", arrCards);
+
+        if (!draggingCard) return;
+        const currentCard = e.currentTarget;
+        const box = currentCard.getBoundingClientRect();
+        const centerBox = box.top + box.height / 2;
+        
+        // const closest = currentCard.closest('.cards')
+
+        console.log("e.clientY + box.height > centerBox", e.clientY, ' + ', box.height, ' > ', centerBox, ' = pos - 1')
+        console.log("e.clientY + box.height <= box.y", e.clientY, ' + ', box.height, ' <= ', box.y, ' = pos + 1')
+        
+        // console.log("e.clientY", e.clientY)
+        // console.log("centerBox", centerBox)
+        // console.log(currentCard.closest('.cards'))
+        // if (card.id === draggingCard.id) return;
+        // slot.classList.add("is-visible");
+
+        if (e.clientY + box.height > centerBox) {
+            console.log("pos - 1");
+            /** видаляється картка, що перетягується */
+            // isDraggingElement?.classList.add('hidden-card');
+
+            /** додаю слот */
+            // currentCard.after(slot);
+        }
+
+        if (e.clientY + box.height <= box.y) {
+            console.log("pos + 1")
+            // currentCard.before(slot);
+            
+        }
+        // console.log(currentCard.closest('.cards'))
+    }
+    
+    function dragLeave(e: any,) {
+        // slot.remove();
+
+        // console.log('Leave', e.currentTarget);
+        // console.log(isDraggingElement);
+        // if (e.currentTarget === isDraggingElement) {
+        //     // isDraggingElement?.classList.add('hidden-card');
+        // }
+        // beforeCard?.classList.remove('is-visible');
+        // afterCard?.classList.remove('is-visible');
+        // slot.remove();
     }
 
     function dragDrop(e: any) {
+        e.preventDefault();
     }
 
     return (
@@ -121,26 +168,27 @@ export const List = (props: {
                     </h2>}
             </div>
             <div className="cards-container">
-                <ul className="cards">
-                    {cards &&
-                        cards.map((card: ICard, index: number) => {
-                            return (
-                                <li
-                                    key={card.id}
-                                    className="drop-zone"
-                                    onDragEnter={(e: any) => dragEnter(e, card)}
-                                >
-                                    {isVisibleSlot[index] &&
-                                        <div className="slot">SLOT</div>
-                                    }
-                                    <div
+                <ol className="cards">
+                    {arrCards &&
+                        arrCards
+                            .sort((a, b) => a.position - b.position)
+                            .map((card: ICard, index: number) => {
+                                return (
+                                    <li
                                         key={card.id}
-                                        id={`${card.id}`}
                                         className="card"
                                         draggable="true"
                                         onDragStart={(e: any) => dragStart(e, card)}
                                         onDragEnd={(e: any) => dragEnd(e)}
+                                        onDragLeave={(e: any) => dragLeave(e)}
+                                        onDragEnter={(e: any) => dragEnter(e)}
+                                        onDrop={(e: any) => dragDrop(expect)}
                                     >
+                                        {/* <div
+                                        key={card.id}
+                                        className="card"
+                                    > */}
+                                        {/* <div className="card-clot-before"></div> */}
                                         <Card
                                             key={card.id}
                                             id={card.id}
@@ -151,12 +199,13 @@ export const List = (props: {
                                             changeTitle={props.changeTitle}
                                             deleteCard={() => props.deleteCard(card.id, "card")}
                                         />
-                                    </div>
-                                </li>
-                            )
-                        }
-                        )}
-                </ul>
+                                        {/* <div className="card-clot-after"></div> */}
+                                        {/* </div> */}
+                                    </li>
+                                )
+                            }
+                            )}
+                </ol>
             </div>
             <div className="add-card-button-container">
                 <CreateNewCard
--- src/store/reducers/SlotSlice.ts
+++ src/store/reducers/SlotSlice.ts
@@ -3,10 +3,24 @@ import { ICard } from "../../common/interfaces/ICard";
 
 interface SlotState{
     draggingCard: ICard | null;
+    coordinatesCard: {
+        top: number,
+        bottom: number,
+        height: number,
+        width: number,
+        y: number,
+    };
 }
 
 const initialState: SlotState  = {
     draggingCard: null,
+    coordinatesCard: {
+        top: 0,
+        bottom: 0,
+        height: 0,
+        width: 0,
+        y: 0,
+}
 };
 
 export const SlotSlice = createSlice({
@@ -15,9 +29,12 @@ export const SlotSlice = createSlice({
     reducers: {
         dragStartReducer(state, action) {
             state.draggingCard = action.payload;
+        },
+        getCoordinatesCard(state, action) {
+            state.coordinatesCard = action.payload;
         }
     },
 })
 
-export const { dragStartReducer } = SlotSlice.actions;
+export const { dragStartReducer, getCoordinatesCard } = SlotSlice.actions;
 export default SlotSlice.reducer;
